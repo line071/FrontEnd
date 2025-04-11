5182213357e7d3415e816aa80baf8022cefae8e0
line071 <aline.b.oliveira8@aluno.senai.sp>
2025-04-11 13:12:56 -0300

    11/04/2025

--- ArrayMatrizes/SA01.js
+++ ArrayMatrizes/SA01.js
@@ -5,14 +5,40 @@ const prompt = require("prompt-sync")();//entrada de dados
 let notas = [];//array de notas
 
 function inserirNotas(){
-    let nota = prompt("Digite a nota: ");
+    let nota = Number(prompt("Digite a nota: "));
     notas.push(nota);
 }
 
 function calcularMedia(){
     let media = notas.reduce((soma,nota)=>soma=nota, 0)/notas.length
+    console.log("A média é "+media)
 }
 
 function menu(){
-    
-}
+    let continuar = true;
+    while (continuar) {
+        console.log("===Calculadora Média===");
+        console.log("|1. Inserir Nota      |");
+        console.log("|2. Calcular Média    |");
+        console.log("|3. Sair              |");
+        console.log("=======================");
+        let operacao = prompt("Escolha a opção desejada: ");
+        switch (operacao) {
+            case "1":
+                inserirNotas();
+                break;
+            case "2":
+                calcularMedia();
+                break;
+            case "3":
+                continuar = false;
+                console.log("Saindo...")
+                break;
+            default:
+                console.log("Essa opção não é válida");
+                break; 
+        } //fim do switch
+    } //fim do while
+} //fim da function
+
+menu();
